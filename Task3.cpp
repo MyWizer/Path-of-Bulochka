73d70541c2004635e18ed97da9c7d1439c19d05e
MyWizer <white.wizer@gmail.com>
2025-12-10 11:59:33 +0300

    Третье задание

---
+++ Task3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+/// <summary>
+/// Вычисление расстояния
+/// </summary>
+/// <param name="v">Скорость движения</param>
+/// <param name="t">Время в пути</param>
+/// <returns>Расстояние всего пути</returns>
+int Distance(double speed, double time)
+{
+	// Здесь будет видна подсказка с комментарием
+	double S = speed * time; // +1 η1
+	return S; // +1 η1
+}
+int main()
+{
+	system("chcp 1251 > null"); 
+
+	double speed, time;
+
+	std::cout << "Введите значение скорости и времени через ПРОБЕЛ: "; 
+	std::cin >> speed >> time; 
+
+	std::cout << "S = " << Distance(speed, time); 
+}
